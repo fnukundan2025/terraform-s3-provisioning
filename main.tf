18cf9f2f9c04676eb8c9bdb7d6b033b03956ec21
fnukundan2025 <kundan.d.singh@capgemini.com>
2026-06-22 02:37:57 -0400

    Rename S3 bucket resource and update tags

--- main.tf
+++ main.tf
@@ -21,11 +21,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "main" {
+  bucket = aws_s3_bucket.example.bucket
 
   tags = {
-    Name        = "Demo S3 Bucket"
+    Name        = "aws_s3_bucket.example.bucket"
     Environment = "Dev"
   }
 }
