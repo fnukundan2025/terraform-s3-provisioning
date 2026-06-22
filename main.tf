7da536e93908e6d61f2ba842ba0808c32dd61259
fnukundan2025 <kundan.d.singh@capgemini.com>
2026-06-21 22:11:27 -0400

    Add Terraform configuration for AWS S3 bucket

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "My-Terraform-Project-demo"
+
+    workspaces {
+      name = "terraform-s3-provisioning"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "Demo S3 Bucket"
+    Environment = "Dev"
+  }
+}
