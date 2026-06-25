87176f0fcf85f2f091ae7cc359043889ec3a2dd7
fnukundan2025 <kundan.d.singh@capgemini.com>
2026-06-24 23:36:38 -0400

    Add S3 bucket versioning to Terraform config
    Updated the Terraform configuration to include S3 bucket versioning and removed redundant bucket resource definition.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,29 @@
 terraform {
-  required_version = ">= 1.5"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+      }
     }
   }
-
-  cloud {
-    organization = "My-Terraform-Project-demo"
-
-    workspaces {
-      name = "terraform-s3-provisioning"
+ 
+  provider "aws" {
+    region = var.aws_region
+  }
+ 
+  resource "aws_s3_bucket" "my_bucket" {
+    bucket = var.bucket_name
+ 
+    tags = {
+      Name        = var.bucket_name
+      Environment = var.environment
     }
   }
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name        = var.bucket_name
-    Environment = "dev"
+ 
+  resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+    bucket = aws_s3_bucket.my_bucket.id
+ 
+    versioning_configuration {
+      status = "Enabled"
+    }
   }
-}
